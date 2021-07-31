b056b752021ef1d4602013e3c37290c72af5ecac
Eason Lin <uu9924079@gmail.com>
2021-07-31 14:07:24 +0800

    init

---
+++ components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Welcome to your nuxt app</h1>
+    <button>Facebook Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home"
+};
+</script>
---
+++ pages/auth/facebook/callback/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>請稍候...</div>
+</template>
+
+<script>
+export default {
+  name: "AuthFacebookCallback"
+};
+</script>
